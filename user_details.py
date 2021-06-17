57b60fcfc082f3497185de69b1d2c520ba2a4e22
Aadityan <52703882+root2700@users.noreply.github.com>
2021-06-17 21:19:03 +0530

    Add files via upload

---
+++ user_details.py
@@ -0,0 +1,44 @@
+import requests
+from PIL import Image
+url='https://codeforces.com/api/user.info?handles='
+
+#taking the codeforces handle as input
+username=input()
+
+link=url+username
+response = requests.get(link)
+json_response = response.json()
+repository = json_response['result'][0]
+try:
+    name=f'{repository["firstName"]}'+" "+f'{repository["lastName"]}' 
+    print("Name: ",name)
+except:
+    print("Name: Not available")
+try:
+    print(f'Rank: {repository["rank"]}') 
+except:
+    print("Rank: Not available")
+try:
+    print(f'Rating: {repository["rating"]}') 
+except:
+    print("Rating: Not available")
+try:
+    print(f'City: {repository["city"]}') 
+except:
+    print("City: Not available")
+try:
+    print(f'Country: {repository["country"]}') 
+except:
+    print("Country: Not available")
+try:
+    print(f'Organization: {repository["organization"]}')    
+except:
+    print("Organization: Not available")
+      
+
+url=f'{repository["titlePhoto"]}'
+
+#displaying the profile picture
+im = Image.open(requests.get(url, stream=True).raw)
+im.show()
+      
